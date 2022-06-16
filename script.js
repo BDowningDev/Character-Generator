7c4d8834e9929a591958683e749d9ef3cd1b3cd1
BDowningDev <bdowningdev@gmail.com>
2022-06-16 15:50:17 -0700

    added object with arrys of message components

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //init
+
+const lifeList = {
+  time: ['modern', 'medieval', 'near future', 'distant future'],
+  occupation: ['soldier', 'teacher', 'pilot', 'police officer', 'student', 'dentist', 'doctor', 'janitor'],
+  age: ['kid', 'teen', 'adult', 'middle aged', 'old'],
+  gender: ['male', 'female']
+}
